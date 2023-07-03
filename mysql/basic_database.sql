59af60b7788fb63a169fee459fb9d20a8d07a674
advanced-rising <vcjsm2283@gmail.com>
2023-07-03 23:02:26 +0900

    start db

---
+++ mysql/basic_database.sql
@@ -0,0 +1,17 @@
+-- 데이터베이스 목록
+show databases;
+
+-- 데이터베이스 생성
+create database test;
+
+-- 데이터베이스 삭제
+drop database test;
+
+-- 데이터베이스 사용
+USE test;
+
+-- 데이터베이스 선택
+SELECT DATABASE();
+
+-- 테이블 목록
+show tables;
