4627225e69125e33473a2c76ac9a81189a3ae013
advanced-rising <vcjsm2283@gmail.com>
2023-07-04 21:16:22 +0900

    create table

--- mysql/basic_database.sql
+++ mysql/basic_database.sql
@@ -15,3 +15,19 @@ SELECT DATABASE();
 
 -- 테이블 목록
 show tables;
+
+-- 테이블 생성
+CREATE TABLE cats (
+  name varchar(50),
+  age int
+);
+
+-- 테이블 생성
+CREATE TABLE dogs (
+	name VARCHAR(50),
+    breed VARCHAR(50),
+    age INT
+);
+
+-- 테이블 삭제
+DROP TABLE cats;
