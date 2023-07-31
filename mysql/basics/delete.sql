0d0d5c80d4c4314270db66540e99cff8670f0b70
advanced-rising <vcjsm2283@gmail.com>
2023-07-31 22:40:41 +0900

    delete - 2

--- mysql/basics/delete.sql
+++ mysql/basics/delete.sql
@@ -7,3 +7,16 @@ DELETE FROM cats WHERE name='Egg';
 -- cats 테이블의 모든 행 삭제하기:
 
 DELETE FROM cats;
+
+-- 4살짜리 고양이 모두 삭제하기:
+
+DELETE FROM cats WHERE age=4;
+-- cat_id가 나이와 같은 모든 고양이 삭제하기:
+
+DELETE FROM cats WHERE cat_id=age;
+
+
+
+-- 모든 고양이 삭제하기:
+
+DELETE FROM cats;
